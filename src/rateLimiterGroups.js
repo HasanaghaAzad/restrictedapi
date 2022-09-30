6fac37914a67c30efe4d0b658e8d25940bbd4eb5
Hasanagha Azad <hasanaghaazad@gmail.com>
2022-09-30 20:38:26 +0300

    added applyRateLimiter and shortened limiterPublic & limiterPrivate

--- src/rateLimiterGroups.js
+++ src/rateLimiterGroups.js
@@ -1,50 +1,41 @@
 const rateLimiter = require('./rateLimiter');
 require('dotenv').config();
 
+function applyRateLimiter(req, res, next, weight, key, limit, keySubject) {
+  return rateLimiter({
+    redisConfig: {
+      port: process.env.REDIS_PORT,
+    },
+    weight,
+    key,
+    limit,
+    handler: (req, res) => {
+      const resetDateTime = new Date(req.rateLimit.resetTime);
+      res.status(429).send(
+          `Too many requests created from this ${keySubject}. Next request is possible at ${
+            (resetDateTime.getHours() < 10 ? '0' : '') + resetDateTime.getHours()
+          }:${
+            (resetDateTime.getMinutes() < 10 ? '0' : '') + resetDateTime.getMinutes()
+          }`,
+      );
+    },
+    req,
+    res,
+    next,
+  });
+}
 
 const limiterPublic = (req, res, next, weight) => {
-  return rateLimiter({
-    redisConfig: {
-      port: process.env.REDIS_PORT,
-    },
-    weight,
-    key: req.ip + ':' + req.url,
-    limit: process.env.IP_LIMIT_PER_HOUR,
-    handler: (req, res) => {
-      const resetDateTime = new Date(req.rateLimit.resetTime);
-      res.status(429).send(
-          `Too many requests created from this IP: ${
-            req.ip
-          }. Next request is possible at ${resetDateTime.getHours()}:${
-            (resetDateTime.getMinutes() < 10 ? '0' : '') + resetDateTime.getMinutes()
-          }`,
-      );
-    },
-    req,
-    res,
-    next,
-  });
+  const key = req.ip + ':' + req.url;
+  const keySubject = 'IP';
+  const limit = process.env.IP_LIMIT_PER_HOUR;
+  return applyRateLimiter(req, res, next, weight, key, limit, keySubject);
 };
 const limiterPrivate = (req, res, next, weight) => {
-  return rateLimiter({
-    redisConfig: {
-      port: process.env.REDIS_PORT,
-    },
-    weight,
-    key: res.token + ':' + req.url,
-    limit: process.env.TOKEN_LIMIT_PER_HOUR,
-    handler: (req, res) => {
-      const resetDateTime = new Date(req.rateLimit.resetTime);
-      res.status(429).send(
-          `Too many requests created with this token. Next request is possible at ${resetDateTime.getHours()}:${
-            (resetDateTime.getMinutes() < 10 ? '0' : '') + resetDateTime.getMinutes()
-          }`,
-      );
-    },
-    req,
-    res,
-    next,
-  });
+  const key = res.token + ':' + req.url;
+  const limit = process.env.TOKEN_LIMIT_PER_HOUR;
+  const keySubject = 'token';
+  return applyRateLimiter(req, res, next, weight, key, limit, keySubject);
 };
 
 module.exports = {limiterPublic, limiterPrivate};
